78db316f3251712a6e723178517688b053a034f8
石涛 <shi.tao@belle.com.cn>
2019-07-25 16:41:59 +0800

    1. 上传flink的示例代码, 该示例已经在job manager中运行成功

---
+++ src/main/java/com/belle/platform/data/analysis/WikipediaAnalysis.java
@@ -0,0 +1,86 @@
+package com.belle.platform.data.analysis;
+
+import org.apache.flink.api.common.functions.AggregateFunction;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.connectors.wikiedits.WikipediaEditEvent;
+import org.apache.flink.streaming.connectors.wikiedits.WikipediaEditsSource;
+
+/**
+ * @description flink官方指导示例代码demo
+ *
+ * @author xiaoTaoShi
+ * @date 2019/7/25 14:26
+ * @version 1.0.0
+ * @copyright (C) 2013 WonHigh Information Technology Co.,Ltd 
+ *  All Rights Reserved. 
+ *
+ * The software for the WonHigh technology development, without the 
+ * company's written consent, and any other individuals and 
+ * organizations shall not be used, Copying, Modify or distribute 
+ * the software.
+ */
+
+public class WikipediaAnalysis {
+	
+	public static void main(String[] args) throws Exception {
+		// set up the streaming execution environment
+		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+		
+		// data source from Wikipedia IRC log
+		DataStream<WikipediaEditEvent> edits = env.addSource(new WikipediaEditsSource());
+		
+		KeyedStream<WikipediaEditEvent, String> keyedEdits = edits
+				.keyBy((KeySelector<WikipediaEditEvent, String>) WikipediaEditEvent::getUser);
+		
+		DataStream<Tuple2<String, Long>> result = keyedEdits
+				.timeWindow(Time.seconds(5)).aggregate(
+						new AggregateFunction<WikipediaEditEvent, Tuple2<String, Long>, Tuple2<String, Long>>() {
+							
+							private static final long serialVersionUID = 464958762405412080L;
+							
+							@Override
+							public Tuple2<String, Long> createAccumulator() {
+								return new Tuple2<>("", 0L);
+							}
+							
+							@Override
+							public Tuple2<String, Long> add(WikipediaEditEvent value,
+									Tuple2<String, Long> accumulator) {
+								return new Tuple2<>(value.getUser(), value.getByteDiff() + accumulator.f1);
+							}
+							
+							@Override
+							public Tuple2<String, Long> getResult(Tuple2<String, Long> accumulator) {
+								return accumulator;
+							}
+							
+							@Override
+							public Tuple2<String, Long> merge(Tuple2<String, Long> a, Tuple2<String, Long> b) {
+								return new Tuple2<>(a.f0 + b.f0, a.f1 + b.f1);
+							}
+						});
+		
+		
+		
+/*		DataStream<Tuple2<String, Long>> result = keyedEdits
+				.timeWindow(Time.seconds(5))
+				.fold(new Tuple2<>("", 0L), new FoldFunction<WikipediaEditEvent, Tuple2<String, Long>>() {
+					@Override
+					public Tuple2<String, Long> fold(Tuple2<String, Long> acc, WikipediaEditEvent event) {
+						acc.f0 = event.getUser();
+						acc.f1 += event.getByteDiff();
+						return acc;
+					}
+				});*/
+		
+		result.print();
+		
+		env.execute();
+		
+	}
+}
